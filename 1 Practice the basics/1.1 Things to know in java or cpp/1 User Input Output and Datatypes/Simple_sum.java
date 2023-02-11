8acfe19d68b1162fb6ec1315ab15cd6b82ec123d
Asikur <asikurr817@gmail.com>
2023-02-12 02:19:26 +0600

    simple sum in java

---
+++ 1 Practice the basics/1.1 Things to know in java or cpp/1 User Input Output and Datatypes/Simple_sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Simple_sum {
+	 public static void main(String[] args) {
+		    Scanner inp=new Scanner(System.in);
+		    int A,B,X;
+		    A=inp.nextInt();
+			B=inp.nextInt();
+			X=A+B;
+			System.out.println("X = "+X);
+	
+
+}
+	 }
