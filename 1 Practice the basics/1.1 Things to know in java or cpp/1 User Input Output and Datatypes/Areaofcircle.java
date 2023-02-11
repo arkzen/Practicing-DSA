e6fb9687c1d9e6c0214dce90d5cb45571b022519
Asikur <asikurr817@gmail.com>
2023-02-12 02:31:59 +0600

    Area of circle

---
+++ 1 Practice the basics/1.1 Things to know in java or cpp/1 User Input Output and Datatypes/Areaofcircle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Areaofcircle {
+	public static void main(String[] args) {
+		
+	
+double A,H,R,J,K;
+
+Scanner inp=new Scanner(System.in);
+R=inp.nextInt();
+Scanner inp1=new Scanner(System.in);
+H=inp1.nextInt();
+System.out.println("R="+R);
+System.out.println("H="+H);;
+K=1/2*(R*H);
+System.out.println("K="+K);
+
+A=(1/2d)*(R*H);
+System.out.println("A="+A);
+
+J=(1/2)*(R*H);
+System.out.println("J="+J);
+
+K=(R*H)*(1/2);
+System.out.println("J="+J);
+
+}
+	}
