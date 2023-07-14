56c22d9f6e9fdce245d4658214641f060f64b5b1
Asikur <asikurr817@gmail.com>
2023-07-14 20:30:11 +0600

    pattern 1 solved

---
+++ 1 Practice the basics/1.2 Build-up Logical Thinking/Solutions of Patterns/pattern1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int i,j,n;
+    n=5;
+
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
