d05d78fc3e185ccf70b424204a57c1464f668147
Asikur <asikurr817@gmail.com>
2023-07-15 09:54:52 +0600

    pattern main fun added

---
+++ 1 Practice the basics/1.2 Build-up Logical Thinking/Solutions of Patterns/pattern10.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern(int n){
+   
+  }
+
+
+int main() {
+
+int t,c;
+cin>>t;
+for(c=0;c<t;c++){
+   int n;
+    cin>>n;
+    pattern(n);
+    cout<<endl;
+}
+    return 0;
+}
