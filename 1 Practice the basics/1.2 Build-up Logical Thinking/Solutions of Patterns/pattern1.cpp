3f2f7d41f5a69c0a0a383cbbbdc7fa86dd6f58e0
Asikur <asikurr817@gmail.com>
2023-08-29 18:27:34 +0600

    pattern one updated

--- 1 Practice the basics/1.2 Build-up Logical Thinking/Solutions of Patterns/pattern1.cpp
+++ 1 Practice the basics/1.2 Build-up Logical Thinking/Solutions of Patterns/pattern1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void pattern1(int n){
     int i,j;
  for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
+        for(j=0;j<i;j++){
             cout<<"* ";
         }
         cout<<endl;
